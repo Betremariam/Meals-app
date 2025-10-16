a34c8429cd3ef85deea3cf9f4d25ad27a1b5f283
Betremariam <yihenewbmariam@gmail.com>
2025-10-16 14:32:50 -0500

    fixed the inkwell issue as it was not abel to find material widget and now our container become clickable and give as smooth effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,18 +6,20 @@ class CatagoriesScreen extends StatelessWidget {
   const CatagoriesScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
       ),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(category: category),
-      ],
     );
   }
 }
