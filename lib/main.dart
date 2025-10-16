645c22b48f2ee9075e9bc08500ae60084b7ad274
Betremariam <yihenewbmariam@gmail.com>
2025-10-16 15:17:25 -0500

    the categories screen and widget modified to support the selctor

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Meal app", home: CatagoriesScreen());
+    return MaterialApp(title: "Meal app", home:);
   }
 }
